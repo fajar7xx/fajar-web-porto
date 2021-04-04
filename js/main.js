48040192a3b153464a2867432ad8877ac748505a
fajarsiagian <fajar7xx@gmail.com>
2021-04-04 22:59:56 +0700

    subsribe

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(function(){
+  let btns = $(".project-area .button-group button")
+  btns.click(function(e){
+    $(".project-area .button-group button").removeClass('active')
+    e.target.classList.add('active')
+
+    let selector = $(e.target).attr('data-filter')
+    $('.project-area .grid').isotope({
+      filter: selector
+    })
+
+    return false
+  })
+
+  $('.project-area .button-group #btn1').trigger('click');
+
+  // magnific popup
+  $('.project-area .grid .popup-link').magnificPopup({
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+  })
+
+  // owl carousel
+  $('.site-main .about-area .owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: true,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      544:{
+        items: 2
+      }
+    }
+  })
+
+})
